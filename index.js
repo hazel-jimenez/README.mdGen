14fc5d91e4f410fd064024b564e4fa93edd9f236
hazel-jimenez <95867111+hazel-jimenez@users.noreply.github.com>
2022-03-04 15:53:23 -0500

    adding js

---
+++ index.js
@@ -0,0 +1,54 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdowns")
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title?'
+    },
+    {
+      type: 'input',
+      message: 'Describe your project:',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'How do you install your project?'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How do you use your project?'
+    }
+    
+  ];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, (data), err =>
+        err ? console.log(err) : console.log('Success!')
+      );
+}
+
+// TODO: Create a function to initialize app
+function init() {   
+    inquirer
+    .prompt(questions)
+    .then(data => {
+  console.log(data)
+      writeToFile('README.md', generateMarkdown(data))
+    });
+}
+
+// Function call to initialize app
+init();
+
+
+
+
+
--- utils/generateMarkdowns.js
+++ utils/generateMarkdowns.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { 
+return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,7 +15,12 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## description
+${data.description}
+## installation
+${data.installation}
 
+${renderLicenseBadge()}
 `;
 }
 
