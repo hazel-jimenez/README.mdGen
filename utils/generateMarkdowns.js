231ebcfe58ba02ce3e65c22fb9932d688f53e5e9
hazel-jimenez <95867111+hazel-jimenez@users.noreply.github.com>
2022-04-27 19:53:51 -0400

    added missing question section and table of contents

--- utils/generateMarkdowns.js
+++ utils/generateMarkdowns.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) { 
-return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)](https://opensource.org/licenses/${license})`
 }
 
 // TODO: Create a function that returns the license link
@@ -15,12 +15,34 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-## description
+  ${renderLicenseBadge(data.licenses)}
+## Description
 ${data.description}
-## installation
-${data.installation}
+## Table of Content
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Test](#test)
+* [Questions](#questions)
 
-${renderLicenseBadge()}
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Contribution
+${data.contribution}
+## Test
+${data.test}
+
+## questions
+You can contact me through:
+${data.email}
+https://github.com/${data.github}
+
+
+
+
+
 `;
 }
 
